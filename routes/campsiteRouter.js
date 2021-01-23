d969f7edd30746e2ceb66aac0b1009402abd9217
noahb <noahb@emeraldaire.com>
2021-01-23 07:44:26 -0800

    Fixed Minae's Typo

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -18,7 +18,7 @@ campsiteRouter.route('/')
     })
     .catch(err => next(err));
 })
-.post((cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Campsite.create(req.body)
     .then(campsite => {
         console.log('Campsite Created ', campsite);
